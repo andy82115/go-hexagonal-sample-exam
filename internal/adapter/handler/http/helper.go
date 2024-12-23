51eafb62808aef587b658a78a0b047f857483b6a
andy82115 <andy82115@gmail.com>
2024-12-24 01:24:03 +0900

    middlewareのためのginのミドルウェア

--- internal/adapter/handler/http/helper.go
+++ internal/adapter/handler/http/helper.go
@@ -1,11 +1,20 @@
 package http
 
-import "strconv"
+import (
+	"strconv"
+
+	"github.com/andy82115/go-hexagonal-sample-exam/internal/core/domain"
+	"github.com/gin-gonic/gin"
+)
 
 func stringToUint64(str string) (uint64, error) {
 	num, err := strconv.ParseUint(str, 10, 64)
 
 	return num, err
+}
+
+func getAuthPayload(ctx *gin.Context, key string) *domain.TokenPayload {
+	return ctx.MustGet(key).(*domain.TokenPayload)
 }
 
 // toMap is a helper function to add meta and data to a map
